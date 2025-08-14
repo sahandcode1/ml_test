ccb988d06b0b25ccb40050dbfdb9a883fa1a8472
sahandcode1 <sahandderakhshani001@gmail.com>
2025-08-14 23:54:17 +0330

    Create business_rules.py

---
+++ Lead_Scoring/src/business_rules.py
@@ -0,0 +1,11 @@
+def apply_business_rules(data,prediction):
+    score = prediction
+
+    if data['region'] == 'Tehran' and data['total_purchases']> 0:
+        score = 1
+    
+    if data['last_interaction_days'] >100:
+        score = 0
+    
+    return score
+
